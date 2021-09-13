6dc9f7a4c64bfa5bbe57cf8dace1f639475d3151
fuatguzel <fuatguzel754@gmail.com>
2021-09-14 01:28:04 +0300

    Add reactstrap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import { Button } from 'reactstrap';
+
+export default function App(props){
+  const notify = () => toast("Wow so easy!");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Button color="danger" onClick={notify}>Notify!</Button>
+      <ToastContainer />
     </div>
   );
 }
-
-export default App;
